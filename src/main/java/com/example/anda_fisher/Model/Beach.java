8ce6b8e858bd6662420f2e6eb51b2e6fa68ac94b
Artur-Sultanov <arturo.sultanov@gmail.com>
2024-06-23 19:58:16 +0200

    fixtypo

--- src/main/java/com/example/anda_fisher/Model/Beach.java
+++ src/main/java/com/example/anda_fisher/Model/Beach.java
@@ -10,9 +10,20 @@ public class Beach {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String location;
+    private String description;
+
+    public Beach(Long id, String name, String location, String description, Set<Fish> fish) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.description = description;
+        this.fish = fish;
+    }
+
+    public Beach() {
+    }
 
     @ManyToMany
     @JoinTable(
@@ -22,35 +33,32 @@ public class Beach {
     @JsonIgnore
     private Set<Fish> fish = new HashSet<>();
 
-    // Getters and setters
+
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getLocation() {
         return location;
     }
-
     public void setLocation(String location) {
         this.location = location;
     }
-
+    public String getDescription() { return description; }
+    public void setDescription(String description) {
+        this.description = description;
+    }
     public Set<Fish> getFish() {
         return fish;
     }
-
     public void setFish(Set<Fish> fish) {
         this.fish = fish;
     }
