4465d84d2134a34d3443a2d3a1424d9ce7c3608c
Artur-Sultanov <arturo.sultanov@gmail.com>
2025-01-22 13:15:35 +0100

    Updated `SecurityConfig`:
    - Added specific access control for the `/api/beaches/{id}/uploadImage` endpoint, allowing only users with `ROLE_USER` or `ROLE_ADMIN` to upload images.
    - Refactored the package from `Security` to `Config` for better organization and alignment with application structure.

--- src/main/java/com/example/anda_fisher/Config/SecurityConfig.java
+++ src/main/java/com/example/anda_fisher/Config/SecurityConfig.java
@@ -1,5 +1,7 @@
-package com.example.anda_fisher.Security;
+package com.example.anda_fisher.Config;
 
+import com.example.anda_fisher.Security.JwtAuthenticationFilter;
+import com.example.anda_fisher.Security.JwtService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -41,12 +43,13 @@ public class SecurityConfig {
                                 response.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden")))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Разрешить доступ
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()  // ✅ Доступ к логину разрешен
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/beaches/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/beaches/**").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/api/beaches/**").hasAuthority("ROLE_ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/beaches/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/beaches/{id}/uploadImage").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                         .requestMatchers(HttpMethod.GET, "/api/fish/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/fish/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/fish/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
